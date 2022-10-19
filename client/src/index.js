aced6eb9998ff685c12a45b453f289e175ba18e2
LuukOriginal <luukoriginal@gmail.com>
2022-10-19 17:39:14 +0200

    First Commit

---
+++ client/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router, Route, Routes, Navigate} from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Login/Login';
+import NotFound from './pages/NotFound/NotFound';
+
+var loggedIn = true
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={loggedIn ? <Navigate to="/dashboard" replace /> : <Home />}
+        />
+        <Route
+          path="dashboard"
+          element={loggedIn ? <Dashboard /> : <Navigate to="/login" replace />}
+        />
+        <Route
+          path="login"
+          element={loggedIn ? <Navigate to="/dashboard" replace /> : <Login/>}
+        />
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
