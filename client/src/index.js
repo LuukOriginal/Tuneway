185d20e9bab46c898c5e08cab6719d76283caf9a
LuukOriginal <luukoriginal@gmail.com>
2022-10-19 19:28:50 +0200

    Fixed routing with custom domain

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ var loggedIn = false
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename='/Tuneway'>
+    <Router>
       <Routes>
         <Route
           path="/"
